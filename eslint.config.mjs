1dbb21d29d59770c719a4e568f41d61af54dbb73
juyeongnoh <juyeongnoh@gmail.com>
2025-07-03 10:22:32 +0900

    Chore: naverpay eslint config 설치 및 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import naverpay from "@naverpay/eslint-config";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +13,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...naverpay.configs.node,
+  ...naverpay.configs.typescript,
+  ...naverpay.configs.strict,
+  ...naverpay.configs.packageJson,
 ];
 
 export default eslintConfig;

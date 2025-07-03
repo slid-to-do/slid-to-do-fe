61e082af38a210f939939ef2ec51a6a9de841718
juyeongnoh <juyeongnoh@gmail.com>
2025-07-03 15:19:09 +0900

    Chore: 코드 저장 시 eslint rule 자동 적용 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from 'node:path'
+import {fileURLToPath} from 'node:url'
 
-import naverpay from "@naverpay/eslint-config";
+import {FlatCompat} from '@eslint/eslintrc'
+import naverpay from '@naverpay/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...naverpay.configs.node,
-  ...naverpay.configs.typescript,
-  ...naverpay.configs.strict,
-  ...naverpay.configs.packageJson,
-];
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    ...naverpay.configs.node,
+    ...naverpay.configs.typescript,
+    ...naverpay.configs.strict,
+]
 
-export default eslintConfig;
+export default eslintConfig

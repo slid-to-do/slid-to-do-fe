0c195eff92c034078503c31e9245f0918a98ba54
juyeongnoh <juyeongnoh@gmail.com>
2025-07-08 09:33:10 +0900

    Feat: useModal - 모달 공용 커스텀 훅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import {Geist, Geist_Mono} from 'next/font/google'
 
+import ModalProvider from './providers/modal-provider'
+
 import type {Metadata} from 'next'
+
 import './globals.css'
 
 const geistSans = Geist({
@@ -25,7 +28,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+                <ModalProvider />
+            </body>
         </html>
     )
 }

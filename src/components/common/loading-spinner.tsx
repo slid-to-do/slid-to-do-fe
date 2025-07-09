58c93ccf82b19face3dbb2874634e27431ab5adb
httpo <83061447+httpo@users.noreply.github.com>
2025-07-09 11:52:42 +0900

    [Chore] 민감 파일 제거 및 코드정리 (#38)
    * Feat: infiniteScroll
    
    * chore: npm 모듈 설치

---
+++ src/components/common/loading-spinner.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSpinner() {
+    return (
+        <>
+            <div className="sticky top-1/2 -translate-y-1/2 z-30 flex justify-center pointer-events-none">
+                <div className="w-8 h-8 rounded-full border-2 border-blue-500 border-t-transparent animate-spin" />
+            </div>
+        </>
+    )
+}

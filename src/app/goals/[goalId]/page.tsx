5b2df6c412a8fbbec1436690772f287fc7306615
httpo <uhagodqhr94@naver.com>
2025-07-15 14:29:11 +0900

    Feat: 삭제 모달 컴포넌트 추가

--- src/app/goals/[goalId]/page.tsx
+++ src/app/goals/[goalId]/page.tsx
@@ -1,18 +1,23 @@
 'use client'
 import Image from 'next/image'
 import {useParams, useRouter} from 'next/navigation'
-import {useCallback, useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 
 import {useMutation, useQueryClient} from '@tanstack/react-query'
 
 import LoadingSpinner from '@/components/common/loading-spinner'
+import TwoButtonModal from '@/components/common/modal/two-buttom-modal'
 import TodoItem from '@/components/common/todo-item'
+import ButtonStyle from '@/components/style/button-style'
+import InputStyle from '@/components/style/input-style'
 import {useInfiniteScrollQuery} from '@/hooks/use-infinite-scroll'
+import useModal from '@/hooks/use-modal'
 import {del, get, patch} from '@/lib/api'
+import {useModalStore} from '@/store/use-modal-store'
 
 import ProgressBar from './prograss-motion'
 
-import type {GoalResponse, GoalProgress} from '@/types/goals'
+import type {Goal, GoalProgress} from '@/types/goals'
 import type {TodoResponse} from '@/types/todos'
 
 const TOKEN = process.env.NEXT_PUBLIC_TEST_TOKEN
@@ -20,8 +25,9 @@ const TEAM_ID = process.env.NEXT_PUBLIC_TEAM_ID
 
 export default function GoalsPage() {
     const router = useRouter()
-    const [posts, setPosts] = useState<GoalResponse>()
+    const [posts, setPosts] = useState<Goal>()
     const [moreButton, setMoreButton] = useState<boolean>(false)
+    const [goalEdit, setGoalEdit] = useState<boolean>(false)
     const [progress, setProgress] = useState<number>(0)
 
     const queryClient = useQueryClient()
@@ -34,7 +40,7 @@ export default function GoalsPage() {
         const getGoalsData = async () => {
             const url = `${TEAM_ID}/goals/${goalId}`
             try {
-                const response = await get<GoalResponse>({
+                const response = await get<Goal>({
                     endpoint: `${url}`,
                     options: {
                         headers: {Authorization: `Bearer ${TOKEN}`},
@@ -113,27 +119,69 @@ export default function GoalsPage() {
         fetchFn: GetTodoList(false),
     })
 
-    const [goalEdit, setGoalEdit] = useState<boolean>(false)
-    /** 목표 수정&삭제 */
-    const handleGoalAction = useCallback((mode: string) => {
-        if (mode === 'edit') {
-            setGoalEdit(true)
-            // router.push(`/goals/${goalId}/edit`)
-            alert('수정되었습니다.')
-        } else if (mode === 'delete' && confirm('정말 삭제하시겠습니까?')) {
-            alert('삭제되었습니다.')
-            /**  fetch(`/api/goals/${goalId}`, {
-                 method: 'DELETE',
-             }).then(() => {
-                 alert('삭제되었습니다.')
-             }) */
-        }
-    }, [])
-
     /** 노트 모아보기 페이지 이동 */
     const goNoteList = () => {
         router.push('/')
     }
+
+    const {clearModal} = useModalStore()
+
+    /** 목표 수정&삭제 */
+    const handleGoalAction = async (mode: string) => {
+        if (mode === 'edit') {
+            if (posts?.title === '') {
+                alert('제목을 입력해주세요.')
+                return
+            }
+            const response = await patch({
+                endpoint: `${TEAM_ID}/goals/${goalId}`,
+                data: {title: posts?.title},
+                options: {
+                    headers: {
+                        Authorization: `Bearer ${TOKEN}`,
+                    },
+                },
+            })
+            if (response.status === 200) {
+                alert('수정되었습니다.')
+            } else {
+                alert(response.message)
+            }
+
+            setGoalEdit(false)
+        } else if (mode === 'delete') {
+            const response = await del({
+                endpoint: `${TEAM_ID}/goals/${goalId}`,
+                options: {
+                    headers: {
+                        Authorization: `Bearer ${TOKEN}`,
+                    },
+                },
+            })
+            if (response === undefined) {
+                clearModal()
+                alert('삭제가 완료되었습니다.')
+                router.push('/')
+            }
+        }
+    }
+
+    /** 목표 삭제 모달 */
+    const {openModal: goalDeleteModal} = useModal(
+        () => (
+            <TwoButtonModal
+                handleLeftBtn={clearModal}
+                handleRightBtn={() => handleGoalAction('delete')}
+                topText={'목표를 삭제하시겠어요?'}
+                bottomText={'삭제된 목표는 복구할 수 없습니다.'}
+                buttonText={'삭제'}
+            />
+        ),
+        {
+            modalAnimation: 'slideFromTop',
+            backdropAnimation: 'fade',
+        },
+    )
 
     const updateTodo = useMutation({
         mutationFn: async ({todoId, newDone}: {todoId: number; newDone: boolean}) => {
@@ -153,7 +201,6 @@ export default function GoalsPage() {
             queryClient.invalidateQueries({queryKey: ['todos']})
         },
     })
-
     const deleteTodo = useMutation({
         mutationFn: async (todoId: number) => {
             if (!confirm('정말로 이 할 일을 삭제하시겠습니까?')) return
@@ -166,13 +213,29 @@ export default function GoalsPage() {
                     },
                 },
             })
-
             return response
         },
         onSuccess: () => {
             queryClient.invalidateQueries({queryKey: ['todos']})
         },
     })
+
+    /** 할일 삭제 모달 */
+    const {openModal: todoDeleteModal} = useModal(
+        (todoId: number) => (
+            <TwoButtonModal
+                handleLeftBtn={clearModal}
+                handleRightBtn={() => deleteTodo.mutate(todoId)}
+                topText={'목표를 삭제하시겠어요?'}
+                bottomText={'삭제된 목표는 복구할 수 없습니다.'}
+                buttonText={'삭제'}
+            />
+        ),
+        {
+            modalAnimation: 'slideFromTop',
+            backdropAnimation: 'fade',
+        },
+    )
 
     let error
     if (doneIsError) {
@@ -188,19 +251,38 @@ export default function GoalsPage() {
             </div>
         )
     }
+    const handleInputUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target
 
+        setPosts((previous) => ({
+            ...previous,
+            title: value,
+            id: previous?.id ?? 0,
+        }))
+    }
     return (
         <div className="min-h-screen bg-custom_slate-100 ">
             <div className="p-4 mx-auto">
                 <div className="text-subTitle">목표</div>
                 <div className="mt-4 py-4 px-6 bg-white rounded">
                     <div className="flex justify-between items-center">
-                        <div className="flex gap-2 items-center">
+                        <div className="flex flex-grow gap-2 items-center">
                             <Image src="/goals/flag-goal.svg" alt="목표깃발" width={40} height={40} />
                             {posts ? (
                                 <>
                                     {goalEdit ? (
-                                        <div className="text-custom_slate-800 font-semibold">수정하세욧</div>
+                                        <div className="w-full flex gap-3 items-center">
+                                            <InputStyle
+                                                type="text"
+                                                placeholder="할 일의 제목을 적어주세요"
+                                                value={posts.title}
+                                                name="title"
+                                                onChange={handleInputUpdate}
+                                            />
+                                            <ButtonStyle size={'medium'} onClick={() => handleGoalAction('edit')}>
+                                                수정
+                                            </ButtonStyle>
+                                        </div>
                                     ) : (
                                         <div className="text-custom_slate-800 font-semibold">{posts.title}</div>
                                     )}
@@ -209,25 +291,31 @@ export default function GoalsPage() {
                                 <div className="text-custom_slate-800 font-semibold">loading...</div>
                             )}
                         </div>
-                        <div className="cursor-pointer relative" onClick={() => setMoreButton(!moreButton)}>
-                            <Image src={'/goals/Meatballs_menu.svg'} alt="더보기버튼" width={24} height={24} />
-                            {moreButton && (
-                                <div className="w-24 py-2 absolute right-0 top-7 flex gap-2 flex-col rounded text-center shadow-md z-10 bg-white">
-                                    <button type="button" onClick={() => handleGoalAction('edit')}>
-                                        수정하기
-                                    </button>
-                                    <button type="button" onClick={() => handleGoalAction('delete')}>
-                                        삭제하기
-                                    </button>
-                                </div>
-                            )}
-                        </div>
+                        {!goalEdit && (
+                            <div
+                                className="flex-shrink-0 cursor-pointer relative"
+                                onClick={() => setMoreButton(!moreButton)}
+                            >
+                                <Image src={'/goals/Meatballs_menu.svg'} alt="더보기버튼" width={24} height={24} />
+                                {moreButton && (
+                                    <div className="w-24 py-2 absolute right-0 top-7 flex gap-2 flex-col rounded text-center shadow-md z-10 bg-white">
+                                        <button type="button" onClick={() => setGoalEdit(true)}>
+                                            수정하기
+                                        </button>
+                                        <button type="button" onClick={goalDeleteModal}>
+                                            삭제하기
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        )}
                     </div>
                     <div className="mt-6 text-subBody font-semibold">Progress</div>
                     <div className="mt-3.5">
                         <ProgressBar progress={progress} />
                     </div>
                 </div>
+
                 <div
                     className="mt-6 py-4 px-6 bg-custom_blue-100 flex items-center justify-between rounded-xl cursor-pointer"
                     onClick={() => goNoteList()}
@@ -262,7 +350,7 @@ export default function GoalsPage() {
                                                         onToggle={(todoId: number, newDone: boolean) =>
                                                             updateTodo.mutate({todoId, newDone})
                                                         }
-                                                        onDelete={(todoId: number) => deleteTodo.mutate(todoId)}
+                                                        onDelete={(todoId: number) => todoDeleteModal(todoId)}
                                                     />
                                                 </div>
                                             ))}

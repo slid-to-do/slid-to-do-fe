48869f5ad015cea2d32725ba5ddedd87489ac7ca
yezzi <unrtwww@gmail.com>
2025-07-16 14:42:57 +0900

    [Feature] 노트 작성/수정 페이지&컴포넌트

---
+++ src/app/notes/write/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+// import NoteEditCompo from '@/components/notes/edit'
+// import NoteWriteCompo from '@/components/notes/write'
+
+const NoteWritePage = async ({searchParams}: {searchParams: Promise<{noteId?: string}>}) => {
+    const {noteId} = await searchParams
+    const isEdit = typeof noteId === 'string'
+
+    return (
+        <div className="flex flex-col max-w-[792px] min-h-screen pt-6 pr-6 pb-6 pl-20">
+            <h1 className="text-subTitle text-custom_slate-900">{isEdit ? '노트 수정' : '노트 작성'}</h1>
+            <div>
+                <Image src="/goals/flag-goal.png" alt="목표깃발" width={24} height={24} />
+                {/* <h2 className="text-subTitle-sm">{notesData?.[0]?.goal.title}</h2> */}
+            </div>
+            <div className="pt-3 flex items-center gap-2">
+                <div className="p-1 bg-custom_slate-100 text-custom_slate-700 text-subBody font-medium rounded-sm">
+                    <p>To do</p>
+                </div>
+                {/* <p className=" text-custom_slate-700 text-subBody font-normal"> {note.todo.title}</p> */}
+            </div>
+            {/* {isEdit ? <NoteEditCompo noteId={noteId!} /> : <NoteWriteCompo />} */}
+        </div>
+    )
+}
+
+export default NoteWritePage

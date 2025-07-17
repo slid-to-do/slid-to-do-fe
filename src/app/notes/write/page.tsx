7faa764230d0b9d7841b34ef950b880a79017921
httpo <83061447+httpo@users.noreply.github.com>
2025-07-17 15:28:57 +0900

    [Feature] 노트 작성하기 중 임시저장 (#75)
    * docs: GitHub 협업 및 브랜치 관리 정책 문서 추가
    
    * Feat: 목표 상세 페이지 기본 구조
    
    * Feat: 목표 및 할일 api 연동 후 무한스크롤 기능 추가
    
    * Feat: 목표 상세 web화면 퍼블리싱 완료
    
    * Feat: TodoItem 컴포넌트 적용 및 타입정리
    
    * Feat: TanStack Query npm install
    
    * Feat: type 수정
    
    * Chore: eslint 수정
    
    * Feat: 목표 수정 화면 및 이미지 변경(png->svg)
    
    * Feat: 삭제 모달 컴포넌트 추가
    
    * Chore: 코드 정리 및 svgr/webpack설치
    
    * chore: update dependencies
    
    * Chore: layout 추가
    
    * chore: 컴포넌트분리
    
    * Fix: add package-lock.json file
    
    * fix:add package-lock.json
    
    * Fix: npm install
    
    * Fix: npm install
    
    * Feat: 5분마다 작성노트 임시저장 기능
    
    ---------
    
    Co-authored-by: Solmi Park <69065439+Parksolmi@users.noreply.github.com>

--- src/app/notes/write/page.tsx
+++ src/app/notes/write/page.tsx
@@ -1,26 +1,126 @@
+'use client'
+
 import Image from 'next/image'
+import {useSearchParams} from 'next/navigation'
+import {useCallback, useEffect, useState} from 'react'
 
 import NoteEditCompo from '@/components/notes/edit'
 import NoteWriteCompo from '@/components/notes/write'
+import ButtonStyle from '@/components/style/button-style'
 
-const NoteWritePage = async ({searchParams}: {searchParams: Promise<{noteId?: string}>}) => {
-    const {noteId} = await searchParams
+const NoteWritePage = () => {
+    const searchParameters = useSearchParams()
+    const goalId = searchParameters.get('goalId')
+    const todoId = searchParameters.get('todoId')
+    const noteId = searchParameters.get('noteId')
+
+    const [toast, setToast] = useState<boolean>(false)
+    const [content, setContent] = useState<string>('')
+    const [subject, setSubject] = useState<string>('')
+
     const isEdit = typeof noteId === 'string'
 
+    useEffect(() => {
+        if (todoId === undefined || goalId === undefined) {
+            alert('확인 할 데이터가 없습니다.') // 에러페이지로 이동
+        } else {
+            if (noteId === null && localStorage.getItem(`note-draft-${goalId}-${todoId}`) !== null) {
+                setToast(true)
+            }
+        }
+    }, [todoId, goalId, noteId])
+
+    /**임시저장 */
+    const saveToLocalStorage = useCallback(
+        (editContent: string) => {
+            if ((content === '<p></p>' || content === '') && subject === '') {
+                alert('제목 또는 내용을 입력해주세요.')
+            }
+            const key = `note-draft-${goalId}-${todoId}`
+            const value = JSON.stringify({
+                goalId,
+                todoId,
+                editContent,
+                editSubject: subject,
+            })
+            localStorage.setItem(key, value)
+        },
+        [goalId, todoId, subject, content],
+    )
+
+    useEffect(() => {
+        if (!goalId || !todoId) return
+        if ((content === '<p></p>' || content === '') && subject === '') return
+        const interval = setInterval(
+            () => {
+                saveToLocalStorage(content)
+            },
+            1 * 60 * 1000,
+        )
+
+        return () => clearInterval(interval)
+    }, [goalId, todoId, content, subject, saveToLocalStorage])
+
     return (
-        <div className="flex flex-col max-w-[792px] min-h-screen pt-6 pr-6 pb-6 pl-20">
-            <h1 className="text-subTitle text-custom_slate-900">{isEdit ? '노트 수정' : '노트 작성'}</h1>
-            <div>
-                <Image src="/goals/flag-goal.svg" alt="목표깃발" width={24} height={24} />
-              <h2 className="text-subTitle-sm">원래는 notesData?.[0]?.goal.title을 받아 나타나야 함</h2>
+        <div className="flex flex-col w-full min-h-screen p-6 desktop:px-20">
+            <div className="w-full flex justify-between items-center">
+                <h1 className="text-subTitle text-custom_slate-900">{isEdit ? '노트 수정' : '노트 작성'}</h1>
+                <div className="flex gap-2">
+                    <ButtonStyle
+                        className="w-24 !font-normal rounded-xl border-0"
+                        color="outline"
+                        onClick={() => saveToLocalStorage(content)}
+                    >
+                        임시작성
+                    </ButtonStyle>
+                    <ButtonStyle className="w-24 bg-custom_slate-400 !font-normal rounded-xl">작성완료</ButtonStyle>
+                </div>
             </div>
-            <div className="pt-3 flex items-center gap-2">
+            {toast && (
+                <div className="mt-4 py-3 px-4 rounded-full bg-custom_blue-50 flex justify-between items-center gap-3">
+                    <div className="flex items-center gap-4">
+                        <Image
+                            src="/notes/ic_delete.svg"
+                            alt="닫기버튼"
+                            width={24}
+                            height={24}
+                            onClick={() => setToast(false)}
+                            className="cursor-pointer"
+                        />
+                        <div className="text-custom_blue-500">
+                            임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?
+                        </div>
+                    </div>
+                    <ButtonStyle color="outline" className="py-2 px-4 w-[84px] rounded-full text-sm">
+                        불러오기
+                    </ButtonStyle>
+                </div>
+            )}
+
+            <div className="mt-4 flex items-center gap-2">
+                <Image src="/goals/flag-goal.svg" alt="목표깃발" width={24} height={24} />
+                <h2 className="text-subTitle-sm">원래는 notesData?.[0]?.goal.title을 받아 나타나야 함</h2>
+            </div>
+            <div className="mt-3 flex items-center gap-2">
                 <div className="p-1 bg-custom_slate-100 text-custom_slate-700 text-subBody font-medium rounded-sm">
                     <p>To do</p>
                 </div>
-               <p className=" text-custom_slate-700 text-subBody font-normal"> 원래는 note.todo.title을 받아 나타나야함</p> 
+                <p className=" text-custom_slate-700 text-subBody font-normal">
+                    원래는 note.todo.title을 받아 나타나야함
+                </p>
             </div>
-            {isEdit ? <NoteEditCompo noteId={noteId!} /> : <NoteWriteCompo />}
+            <div className="mt-6">
+                {isEdit ? (
+                    <NoteEditCompo noteId={noteId!} />
+                ) : (
+                    <NoteWriteCompo
+                        content={content}
+                        setContent={setContent}
+                        subject={subject}
+                        setSubject={setSubject}
+                    />
+                )}
+            </div>
         </div>
     )
 }

6478caae1141c0cc02b3ba7c7c4f42472a26cd23
httpo <83061447+httpo@users.noreply.github.com>
2025-07-21 13:43:15 +0900

    [Chore] 목표 퍼센트 및 무한스크롤 ref 수정 (#81)
    * docs: GitHub 협업 및 브랜치 관리 정책 문서 추가
    
    * Feat: 목표 상세 페이지 기본 구조
    
    * Feat: 목표 및 할일 api 연동 후 무한스크롤 기능 추가
    
    * Feat: 목표 상세 web화면 퍼블리싱 완료
    
    * Feat: TodoItem 컴포넌트 적용 및 타입정리
    
    * Feat: TanStack Query npm install
    
    * Feat: type 수정
    
    * Chore: eslint 수정
    
    * Feat: 목표 수정 화면 및 이미지 변경(png->svg)
    
    * Feat: 삭제 모달 컴포넌트 추가
    
    * Chore: 코드 정리 및 svgr/webpack설치
    
    * chore: update dependencies
    
    * Chore: layout 추가
    
    * chore: 컴포넌트분리
    
    * Fix: add package-lock.json file
    
    * fix:add package-lock.json
    
    * Fix: npm install
    
    * Fix: npm install
    
    * Feat: 5분마다 작성노트 임시저장 기능
    
    * Chore: api 통신 코드 변경
    
    * Chore: 컴포넌트 코드 변경
    
    * Chore: 목표 퍼센트 수정 및 ref 높이 수정
    
    ---------
    
    Co-authored-by: Solmi Park <69065439+Parksolmi@users.noreply.github.com>

--- src/app/goals/[goalId]/page.tsx
+++ src/app/goals/[goalId]/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import {useParams, useRouter} from 'next/navigation'
 import {useEffect, useState} from 'react'
 
-import {useMutation, useQueryClient} from '@tanstack/react-query'
+import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query'
 
 import AddTodoModal from '@/components/common/modal/add-todo-modal'
 import TwoButtonModal from '@/components/common/modal/two-buttom-modal'
@@ -33,48 +33,48 @@ const GoalsPage = () => {
     const {clearModal} = useModalStore()
 
     /** [ S ] 목표 */
-    /** 목표 API 호출 */
+    /** 목표 API */
+    const {data: goalsData} = useQuery<Goal>({
+        queryKey: ['goals', goalId],
+        queryFn: async () => {
+            const response = await get<Goal>({
+                endpoint: `goals/${goalId}`,
+                options: {
+                    headers: {Authorization: `Bearer ${localStorage.getItem('refreshToken')}`},
+                },
+            })
+
+            return response.data
+        },
+    })
+
+    /** 목표 달성 API */
+    const {data: progressData} = useQuery<GoalProgress>({
+        queryKey: ['todos', goalId, 'progress'],
+        queryFn: async () => {
+            const response = await get<GoalProgress>({
+                endpoint: `todos/progress?goalId=${goalId}`,
+                options: {
+                    headers: {Authorization: `Bearer ${localStorage.getItem('refreshToken')}`},
+                },
+            })
+            return response.data
+        },
+    })
+
     useEffect(() => {
-        const getGoalsData = async () => {
-            const url = `goals/${goalId}`
-            try {
-                const response = await get<Goal>({
-                    endpoint: `${url}`,
-                    options: {
-                        headers: {Authorization: `Bearer ${localStorage.getItem('refreshToken')}`},
-                    },
-                })
-                const goal = response.data
-                setPosts(goal)
-
-                const getProgress = await get<GoalProgress>({
-                    endpoint: `todos/progress`,
-                    options: {
-                        headers: {Authorization: `Bearer ${localStorage.getItem('refreshToken')}`},
-                    },
-                })
-                setProgress(getProgress.data.progress / 100)
-            } catch (error: unknown) {
-                if (error instanceof Error) {
-                    return <div>{error.message}</div>
-                }
-                return <div>{String(error)}</div>
-            }
+        if (goalsData) {
+            setPosts(goalsData)
         }
-
-        if (goalId) {
-            getGoalsData()
+        if (progressData) {
+            setProgress(progressData.progress)
         }
-    }, [goalId])
+    }, [goalsData, progressData])
 
-    /** 목표 수정&삭제 */
-    const handleGoalAction = async (mode: string) => {
-        if (mode === 'edit') {
-            if (posts?.title === '') {
-                alert('제목을 입력해주세요.')
-                return
-            }
-            const response = await patch({
+    /** 목표 수정 */
+    const updateGoals = useMutation({
+        mutationFn: async () => {
+            const response = await patch<TodoResponse>({
                 endpoint: `goals/${goalId}`,
                 data: {title: posts?.title},
                 options: {
@@ -83,14 +83,22 @@ const GoalsPage = () => {
                     },
                 },
             })
+
             if (response.status === 200) {
                 alert('수정되었습니다.')
             } else {
                 alert(response.message)
             }
 
-            setGoalEdit(false)
-        } else if (mode === 'delete') {
+            return response.data
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['goals']})
+        },
+    })
+    /** 목표 삭제 */
+    const deleteGoals = useMutation({
+        mutationFn: async () => {
             const response = await del({
                 endpoint: `goals/${goalId}`,
                 options: {
@@ -104,6 +112,21 @@ const GoalsPage = () => {
                 alert('삭제가 완료되었습니다.')
                 router.push('/')
             }
+        },
+    })
+
+    /** 목표 수정&삭제 분기 함수 */
+    const handleGoalAction = async (mode: string) => {
+        if (mode === 'edit') {
+            if (posts?.title === '') {
+                alert('제목을 입력해주세요.')
+                return
+            }
+            updateGoals.mutate()
+
+            setGoalEdit(false)
+        } else if (mode === 'delete') {
+            deleteGoals.mutate()
         }
     }
 

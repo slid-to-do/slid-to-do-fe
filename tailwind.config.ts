6e75caaa9fda7556c24719427015648850215271
Solmi <7548ck@naver.com>
2025-07-23 17:04:19 +0900

    style: react-tostify 클래스 속성 커스텀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import plugin from 'tailwindcss/plugin'
 import type {Config} from 'tailwindcss'
 
 const config: Config = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/style/**/*.{ts,tsx}'],
     theme: {
         extend: {
             keyframes: {

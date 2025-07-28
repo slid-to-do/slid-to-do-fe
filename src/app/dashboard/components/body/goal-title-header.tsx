b948545f3d5566567ed9a62377b988d9340d28eb
nile27 <ddj03104@gmail.com>
2025-07-28 11:19:43 +0900

    Fix: dashboard, 할 일 추가 모달 설정

--- src/app/dashboard/components/body/goal-title-header.tsx
+++ src/app/dashboard/components/body/goal-title-header.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import Link from 'next/link'
 import React from 'react'
 
 import {useQuery} from '@tanstack/react-query'
 
+import AddTodoModal from '@/components/common/modal/add-todo-modal'
 import ProgressBar from '@/components/goals/prograss-motion'
+import {useModal} from '@/hooks/use-modal'
 import {get} from '@/lib/api'
 
 import type {GoalProgress} from '@/types/goals'
 
 const GoalTitleHeader = ({goalId, title}: {goalId: number; title: string}) => {
+    const {openModal} = useModal(<AddTodoModal />)
     const {data: progressData} = useQuery<number>({
         queryKey: ['todos', goalId, 'dashProgress'],
         queryFn: async () => {
@@ -30,12 +32,12 @@ const GoalTitleHeader = ({goalId, title}: {goalId: number; title: string}) => {
         <header className="w-full h-auto p-2 ">
             <div className="w-full h-auto p-2 flex justify-between items-center">
                 <h1 className="text-title-base font-semibold">{title}</h1>
-                <Link
-                    href={`/goals/${goalId}`}
+                <button
+                    onClick={openModal}
                     className=" text-blue-500 text-subBody-sm font-bold w-auto h-auto cursor-pointer"
                 >
                     +할일 추가
-                </Link>
+                </button>
             </div>
             <ProgressBar progress={progressData || 0} />
         </header>

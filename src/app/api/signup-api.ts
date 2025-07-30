71c967af59df7214653ed1b0b518c6ba6eaef83f
Solmi <7548ck@naver.com>
2025-07-30 16:20:19 +0900

    feat: 회원가입 훅을 api 호출 util 함수로 변경

---
+++ src/app/api/signup-api.ts
@@ -0,0 +1,14 @@
+import {post} from '@/lib/api'
+import type {SignupFormData} from '@/types/signup'
+
+/**
+ * 회원가입 API 요청 함수
+ * @param data - 회원가입 폼 데이터
+ * @returns void (응답 데이터는 사용하지 않음)
+ */
+export async function signupApi(data: SignupFormData): Promise<void> {
+    await post({
+        endpoint: 'user',
+        data,
+    })
+}

2afa344b3aa876f93be7576dd523334963cafc81
Solmi Park <69065439+Parksolmi@users.noreply.github.com>
2025-08-05 17:34:48 +0900

    [Feature] useCustomMutation/Query 수정 (#138)
    * docs: 안 쓰는 파일 삭제
    
    * feat: ErrorDisplayType에 both 추가
    
    * feat: 에러 발생 리다이렉션 페이지에서 토스트 띄우기
    
    * feat: 토스트 메세지 id 지정
    
    * feat: 토스트 메세지 1개로 제한
    
    * feat: 토스트 메세지 중복 방지 로직 추가
    
    * feat: 토스트 메세지 대기큐 비우기 기능 추가
    
    * style: 토스트 메세지 위치 및 배경색 변경
    
    * style: 토스트 메세지 다크모드 적용
    
    * refactor: 함수별 로직 분리하여 Cognitive Complexity 문제 해결
    
    * chore: lint error 해결

--- src/hooks/use-custom-query.ts
+++ src/hooks/use-custom-query.ts
@@ -6,14 +6,14 @@ import {useQuery, type UseQueryOptions, type QueryKey, type QueryFunction} from 
 
 import useToast from '@/hooks/use-toast'
 
-type ErrorDisplayType = 'toast' | 'redirect' | 'component' | 'none'
+type ErrorDisplayType = 'toast' | 'redirect' | 'component' | 'both' | 'none'
 
 type CustomQueryOptions<TData, TError, TQueryKey extends QueryKey, TSelected = TData> = Omit<
     UseQueryOptions<TData, TError, TSelected, TQueryKey>,
     'queryKey' | 'queryFunction'
 > & {
     errorDisplayType?: ErrorDisplayType
-    mapErrorMessage?: (error: unknown) => string
+    mapErrorMessage?: (error: TError) => string
     errorRedirectPath?: string
     onError?: (error: TError) => void
 }
@@ -56,12 +56,15 @@ export function useCustomQuery<TData, TError = unknown, TQueryKey extends QueryK
         queryFn: queryFunction,
         ...rest,
     })
-
     if (queryResult.isError && queryResult.error) {
-        if (errorDisplayType === 'toast') {
-            showToast(mapErrorMessage(queryResult.error), {type: 'error', id: `error-${queryKey.join('-')}`})
-        } else if (errorDisplayType === 'redirect') {
-            router.push(errorRedirectPath)
+        const message = mapErrorMessage(queryResult.error)
+
+        if (['toast', 'both'].includes(errorDisplayType)) {
+            showToast(message, {id: 'QUERY_TOAST_ID', type: 'error'})
+        }
+        if (['redirect', 'both'].includes(errorDisplayType)) {
+            const encodedMessage = encodeURIComponent(message)
+            router.push(`${errorRedirectPath}?toast=${encodedMessage}`)
         }
 
         onError?.(queryResult.error as TError)

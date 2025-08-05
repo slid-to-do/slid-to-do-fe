703fa69eee8f74b9054883c3eeaade973674db9a
nile27 <ddj03104@gmail.com>
2025-08-05 11:20:16 +0900

    Feat: Proxy 서버 구현

---
+++ src/app/api/getproxy/route.ts
@@ -0,0 +1,128 @@
+import {NextResponse} from 'next/server'
+import {cookies} from 'next/headers'
+
+export async function GET(req: Request) {
+    const cookieStore = await cookies()
+    const accessToken = cookieStore.get('refreshToken')?.value
+    const {searchParams} = new URL(req.url)
+    const endpoint = searchParams.get('endpoint') as string
+
+    if (!accessToken) {
+        return NextResponse.json({message: '로그인이 만료 되었습니다.'}, {status: 401})
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/${decodeURIComponent(endpoint)}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        })
+        const data = await response.json()
+
+        return NextResponse.json(data, {status: response.status})
+    } catch (error) {
+        if (error instanceof Error) {
+            const status = (error as Error & {status?: number}).status || 500
+
+            console.error('Proxy Error:', error)
+            return NextResponse.json({message: 'Proxy Error', error: String(error)}, {status})
+        }
+    }
+}
+
+export async function POST(req: Request) {
+    const cookieStore = await cookies()
+    const accessToken = cookieStore.get('refreshToken')?.value
+    const {searchParams} = new URL(req.url)
+    const endpoint = searchParams.get('endpoint') as string
+    const body = await req.json()
+
+    if (!accessToken) {
+        return NextResponse.json({message: '로그인이 만료 되었습니다.'}, {status: 401})
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/${decodeURIComponent(endpoint)}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(body),
+        })
+        const data = await response.json()
+
+        return NextResponse.json(data, {status: response.status})
+    } catch (error) {
+        if (error instanceof Error) {
+            const status = (error as Error & {status?: number}).status || 500
+
+            console.error('Proxy Error:', error)
+            return NextResponse.json({message: 'Proxy Error', error: String(error)}, {status})
+        }
+    }
+}
+
+export async function PATCH(req: Request) {
+    const cookieStore = await cookies()
+    const accessToken = cookieStore.get('refreshToken')?.value
+    const {searchParams} = new URL(req.url)
+    const endpoint = searchParams.get('endpoint') as string
+    const body = await req.json()
+
+    if (!accessToken) {
+        return NextResponse.json({message: '로그인이 만료 되었습니다.'}, {status: 401})
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/${decodeURIComponent(endpoint)}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(body),
+        })
+        const data = await response.json()
+
+        return NextResponse.json(data, {status: response.status})
+    } catch (error) {
+        if (error instanceof Error) {
+            const status = (error as Error & {status?: number}).status || 500
+
+            console.error('Proxy Error:', error)
+            return NextResponse.json({message: 'Proxy Error', error: String(error)}, {status})
+        }
+    }
+}
+
+export async function DELETE(req: Request) {
+    const cookieStore = await cookies()
+    const accessToken = cookieStore.get('refreshToken')?.value
+    const {searchParams} = new URL(req.url)
+    const endpoint = searchParams.get('endpoint') as string
+
+    if (!accessToken) {
+        return NextResponse.json({message: '로그인이 만료 되었습니다.'}, {status: 401})
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/${decodeURIComponent(endpoint)}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        })
+
+        return NextResponse.json({status: response.status})
+    } catch (error) {
+        if (error instanceof Error) {
+            const status = (error as Error & {status?: number}).status || 500
+
+            console.error('Proxy Error:', error)
+            return NextResponse.json({message: 'Proxy Error', error: String(error)}, {status})
+        }
+    }
+}

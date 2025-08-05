336cf56e5fac1edf7e1ff2d6c2caa0cfc4d4c86a
nile27 <ddj03104@gmail.com>
2025-08-05 18:42:11 +0900

    Rename: 화살표 함수로 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import {NextResponse} from 'next/server'
 
 import type {NextRequest} from 'next/server'
 
-export function middleware(request: NextRequest) {
+export const middleware = (request: NextRequest) => {
     const {pathname} = request.nextUrl
     const accessToken = request.cookies.get('accessToken')?.value
 

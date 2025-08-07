c157ea319513a46b4088c911f2e33698f4ac20d9
nile27 <ddj03104@gmail.com>
2025-08-07 16:53:40 +0900

    Fix: middleware config 읽지 못함 버그 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,9 +26,7 @@ export const middleware = (request: NextRequest) => {
         !pathname.endsWith('.svg') &&
         !pathname.endsWith('.ico')
 
-
     if (isProtectedPath && (!accessToken || !refreshToken)) {
-
         return NextResponse.redirect(new URL('/login', request.url))
     }
 
@@ -36,7 +34,5 @@ export const middleware = (request: NextRequest) => {
 }
 
 export const config = {
-
-    matcher: [String.raw`/((?!_next|_next/image|favicon.ico|api|.*\\.svg$).*)`],
-
+    matcher: [`/((?!_next|_next/image|favicon.ico|api|.*\\.svg$).*)`],
 }

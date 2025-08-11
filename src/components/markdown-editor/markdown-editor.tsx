a2155fa53394119ea0248730e2dc7b7caa55c4b8
yezzi <unrtwww@gmail.com>
2025-08-11 17:26:08 +0900

    [Fix] 린트에러 수정

--- src/components/markdown-editor/markdown-editor.tsx
+++ src/components/markdown-editor/markdown-editor.tsx
@@ -15,8 +15,103 @@ import useToast from '@/hooks/use-toast'
 import {MaxLines} from '@/lib/notes/editor/max-lines'
 import {PasteLimiter} from '@/lib/notes/editor/paste-limiter'
 
-
 import LinkModal from '../common/modal/link-modal'
+
+const Toolbar = ({
+    editorInstance,
+    linkButton,
+    onSetLinkButton,
+}: {
+    editorInstance: ReturnType<typeof useEditor>
+    linkButton?: string | undefined
+    onSetLinkButton?: (link: string | undefined) => void
+}) => {
+    /** 링크 modal */
+    const {openModal, closeModal} = useModal(
+        () => (
+            <LinkModal
+                onSetButton={(url) => {
+                    onSetLinkButton?.(url)
+                    closeModal()
+                }}
+            />
+        ),
+        {
+            modalAnimation: 'slideFromTop',
+            backdropAnimation: 'fade',
+        },
+    )
+
+    return (
+        <div className="absolute flex w-full gap-4 p-2 bg-white rounded-full shadow-sm -bottom-24 border-slate-200">
+            <div className="flex gap-1">
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleBold().run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 transition ${editorInstance?.isActive('bold') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-bold.svg" alt="Bold" width={24} height={24} />
+                </button>
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleItalic().run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('italic') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-italic.svg" alt="Italic" width={24} height={24} />
+                </button>
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleUnderline().run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('underline') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-underline.svg" alt="Underline" width={24} height={24} />
+                </button>
+            </div>
+
+            <div className="flex gap-1">
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('left').run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('left') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-align-left.svg" alt="Left Align" width={24} height={24} />
+                </button>
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('center').run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('center') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-align-center.svg" alt="Center Align" width={24} height={24} />
+                </button>
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('right').run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('right') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-align-right.svg" alt="Right Align" width={24} height={24} />
+                </button>
+            </div>
+
+            <div className="flex gap-1">
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleBulletList().run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('bulletList') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-bullet-list.svg" alt="Bullet List" width={24} height={24} />
+                </button>
+                <button
+                    onClick={() => editorInstance?.chain().focus().toggleOrderedList().run()}
+                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('orderedList') ? 'bg-blue-500' : 'bg-white'}`}
+                >
+                    <Image src="/markdown-editor/ic-ordered-list.svg" alt="Ordered List" width={24} height={24} />
+                </button>
+            </div>
+
+            {!linkButton && (
+                <button
+                    onClick={openModal}
+                    className="rounded-full hover:bg-blue-500 size-6 font-bold transition bg-white"
+                >
+                    <Image src="/markdown-editor/ic-link.svg" alt="Link Button" width={24} height={24} />
+                </button>
+            )}
+        </div>
+    )
+}
 
 const MarkdownEditor = ({
     value,
@@ -131,102 +226,5 @@ const MarkdownEditor = ({
         </div>
     )
 }
-
-const Toolbar = ({
-    editorInstance,
-    linkButton,
-    onSetLinkButton,
-}: {
-    editorInstance: ReturnType<typeof useEditor>
-    linkButton?: string | undefined
-    onSetLinkButton?: (link: string | undefined) => void
-}) => {
-    /** 링크 modal */
-    const {openModal, closeModal} = useModal(
-        () => (
-            <LinkModal
-                onSetButton={(url) => {
-                    onSetLinkButton?.(url)
-                    closeModal()
-                }}
-            />
-        ),
-        {
-            modalAnimation: 'slideFromTop',
-            backdropAnimation: 'fade',
-        },
-    )
-
-    return (
-        <div className="absolute flex w-full gap-4 p-2 bg-white rounded-full shadow-sm -bottom-24 border-slate-200">
-            <div className="flex gap-1">
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleBold().run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 transition ${editorInstance?.isActive('bold') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-bold.svg" alt="Bold" width={24} height={24} />
-                </button>
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleItalic().run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('italic') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-italic.svg" alt="Italic" width={24} height={24} />
-                </button>
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleUnderline().run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('underline') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-underline.svg" alt="Underline" width={24} height={24} />
-                </button>
-            </div>
-
-            <div className="flex gap-1">
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('left').run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('left') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-align-left.svg" alt="Left Align" width={24} height={24} />
-                </button>
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('center').run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('center') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-align-center.svg" alt="Center Align" width={24} height={24} />
-                </button>
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleTextAlign('right').run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('right') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-align-right.svg" alt="Right Align" width={24} height={24} />
-                </button>
-            </div>
-
-            <div className="flex gap-1">
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleBulletList().run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('bulletList') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-bullet-list.svg" alt="Bullet List" width={24} height={24} />
-                </button>
-                <button
-                    onClick={() => editorInstance?.chain().focus().toggleOrderedList().run()}
-                    className={`rounded-full hover:bg-blue-500 size-6 font-bold transition ${editorInstance?.isActive('orderedList') ? 'bg-blue-500' : 'bg-white'}`}
-                >
-                    <Image src="/markdown-editor/ic-ordered-list.svg" alt="Ordered List" width={24} height={24} />
-                </button>
-            </div>
-
-            {!linkButton && (
-                <button
-                    onClick={openModal}
-                    className="rounded-full hover:bg-blue-500 size-6 font-bold transition bg-white"
-                >
-                    <Image src="/markdown-editor/ic-link.svg" alt="Link Button" width={24} height={24} />
-                </button>
-            )}
-        </div>
-    )
-}
-
 
 export default MarkdownEditor

5b34c4da3595ab31399e1bb685b9703b2e6b5886
httpo <83061447+httpo@users.noreply.github.com>
2025-08-13 09:31:03 +0900

    [Fix] 프로그래스바 실시간 적용 오류 처리 (#197)
    * docs: GitHub 협업 및 브랜치 관리 정책 문서 추가
    
    * Feat: 로그인 커버페이지 및 function 함수 변경
    
    * Design: 글꼴변경
    
    * Design: 멘토링 피드백 참고 수정
    
    * Fix: 노트 작성하기 에러 해결
    
    * Fix: tab포커스 수정
    
    * Design: 노트작성 임시저장 모바일버전 및 할일리스트 말줄임표
    
    * Design: 로딩스피너 안쪽으로 변경
    
    * Fix: 목표 상세 페이지에서 할일 추가 클릭 시 목표 자동설정 안되는 부분 수정
    
    * Chore: 코드 원본으로 변경
    
    * Fix: 새목표 모달 에러처리
    
    * Fix: 노트 작성하기 버튼 사라짐이슈 해결
    
    * Fix: 목표 수정 시 바로 적용 안되는 것 수정
    
    * Fix: 프로그래스바 실시간적용 오류 처리
    
    * Fix: 목표 상세 페이지 에러처리 both 로 변경
    
    ---------
    
    Co-authored-by: Solmi Park <69065439+Parksolmi@users.noreply.github.com>

--- src/app/goals/[goalId]/page.tsx
+++ src/app/goals/[goalId]/page.tsx
@@ -48,7 +48,7 @@ const GoalsPage = () => {
         ['goal', goalId],
         async () => goalDataApi(goalId),
         {
-            errorDisplayType: 'toast',
+            errorDisplayType: 'both',
             mapErrorMessage: (error) => {
                 const typedError = error as {message?: string; response?: {data?: {message?: string}}}
 
@@ -58,7 +58,7 @@ const GoalsPage = () => {
 
                 return typedError.message || '알 수 없는 오류가 발생했습니다.'
             },
-            // errorRedirectPath: '/',
+            errorRedirectPath: '/',
         },
     )
 
@@ -85,7 +85,6 @@ const GoalsPage = () => {
             },
             onSuccess: () => {
                 queryClient.invalidateQueries({queryKey: ['goal']})
-                queryClient.invalidateQueries({queryKey: ['goals']})
                 showToast('수정이 완료되었습니다.')
             },
         },
@@ -217,6 +216,7 @@ const GoalsPage = () => {
             },
             onSuccess: () => {
                 queryClient.invalidateQueries({queryKey: ['todos']})
+                queryClient.invalidateQueries({queryKey: ['goal']})
             },
         },
     )
@@ -237,6 +237,7 @@ const GoalsPage = () => {
             },
             onSuccess: () => {
                 queryClient.invalidateQueries({queryKey: ['todos']})
+                queryClient.invalidateQueries({queryKey: ['goal']})
             },
         },
     )
